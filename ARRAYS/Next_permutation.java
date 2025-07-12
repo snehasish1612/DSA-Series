f1a0323d5c1125569a4dc885abd6ca2abe96aaa4
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-07-13 00:15:53 +0530

    Create Next_permutation.java

---
+++ ARRAYS/Next_permutation.java
@@ -0,0 +1,67 @@
+// A permutation of an array of integers is an arrangement of its members into a sequence or linear order.
+//   For example, for arr = [1,2,3], the following are all the permutations of arr: [1,2,3], [1,3,2], [2, 1, 3], [2, 3, 1], [3,1,2], [3,2,1].
+// The next permutation of an array of integers is the next lexicographically greater permutation of its integer. 
+// More formally, if all the permutations of the array are sorted in one container according to their lexicographical order, then the next permutation of that array is the permutation that follows it in the sorted container. 
+// If such arrangement is not possible, the array must be rearranged as the lowest possible order (i.e., sorted in ascending order).
+
+//   For example, the next permutation of arr = [1,2,3] is [1,3,2].
+//   Similarly, the next permutation of arr = [2,3,1] is [3,1,2].
+//   While the next permutation of arr = [3,2,1] is [1,2,3] because [3,2,1] does not have a lexicographical larger rearrangement.
+//   Given an array of integers nums, find the next permutation of nums.
+
+// The replacement must be in place and use only constant extra memory.
+
+// Example 1:
+// Input: nums = [1,2,3]
+// Output: [1,3,2]
+  
+// Example 2:
+// Input: nums = [3,2,1]
+// Output: [1,2,3]
+  
+// Example 3:
+// Input: nums = [1,1,5]
+// Output: [1,5,1]
+
+//Solution:- Time Complexity: O(n), Space Complexity: O(1)
+class Solution {
+    public void nextPermutation(int[] nums) {
+        int n = nums.length, pivot = -1;
+        //Find pivot
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
+                pivot = i;
+                break;
+            }
+        }
+        //If array is in decreasing order
+        if(pivot == -1){
+            reverse(nums, 0);
+            return;
+        }
+        //Swap the pivot element with the most right element > nums[pivot]
+        for(int i = n - 1; i > pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(nums, i, pivot);
+                break;
+            }
+        }
+        //Reverse (pivot + 1) to n - 1
+        reverse(nums, pivot + 1);
+    }
+
+    public void reverse(int[] nums, int start){
+        int end = nums.length - 1;
+        while(start <= end){
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+
+    public void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
