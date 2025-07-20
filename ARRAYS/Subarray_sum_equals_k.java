7f188b81c7376965bd977a77d005c795afae0d33
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-07-20 14:27:02 +0530

    Update Subarray_sum_equals_k.java

--- ARRAYS/Subarray_sum_equals_k.java
+++ ARRAYS/Subarray_sum_equals_k.java
@@ -26,3 +26,29 @@ class Solution {
         return count;
     }
 }
+
+//Using Prefix Sum Approach: TC: O(n), SC: O(n)
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, n = nums.length;
+        
+        int[] prefixSum = new int[n];
+        prefixSum[0] = nums[0];
+        for(int i = 1; i < n; i++)
+            prefixSum[i] = prefixSum[i - 1] + nums[i];
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if(prefixSum[i] == k)
+                count++;
+
+            int val = prefixSum[i] - k;
+            if(map.containsKey(val))
+                count += map.get(val);
+            
+            map.put(prefixSum[i], map.getOrDefault(prefixSum[i], 0) + 1);
+        }
+
+        return count;
+    }
+}
