d99664768bb3c32203279a7cf5b2397571a759f8
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-07-20 15:43:56 +0530

    Update Find_Duplicate.java

--- ARRAYS/Find_Duplicate.java
+++ ARRAYS/Find_Duplicate.java
@@ -28,3 +28,23 @@ class Solution {
         return -1;
     }
 }
+
+// Using Slow-Fast Pointer Approach: Time Complexity: O(n), Space Complexity: O(1)
+class Solution {
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0], fast = nums[0];
+
+        do{
+            slow = nums[slow];          //+1
+            fast = nums[nums[fast]];    //+2
+        }while(slow != fast);
+
+        slow = nums[0];
+        while(slow != fast){
+            slow = nums[slow];          //+1
+            fast = nums[fast];          //+1
+        }
+
+        return slow;
+    }
+}
