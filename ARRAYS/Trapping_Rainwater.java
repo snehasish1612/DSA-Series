c1a269f303d569f4560427f5c4d475a867321569
Snehasish Mukherjee <snehasish1612@gmail.com>
2025-07-21 23:54:15 +0530

    Update Trapping_Rainwater.java

--- ARRAYS/Trapping_Rainwater.java
+++ ARRAYS/Trapping_Rainwater.java
@@ -33,3 +33,27 @@ class Solution {
         return ans;
     }
 }
+
+//Using 2-pointer: Time: O(n), Space: O(1)
+class Solution {
+    public int trap(int[] height) {
+        int n = height.length, ans = 0;
+        int l = 0, r = n - 1;
+        int lmax = 0, rmax = 0;
+
+        while(l < r){
+            lmax = Math.max(lmax, height[l]);
+            rmax = Math.max(rmax, height[r]);
+
+            if(lmax < rmax){
+                ans += lmax - height[l];
+                l++;
+            }else{
+                ans += rmax - height[r];
+                r--;
+            }
+        }
+
+        return ans;
+    }
+}
